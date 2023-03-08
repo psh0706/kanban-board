4663048a621ea8cefdb5111cc557287842515c89
psh0706 <best8427@naver.com>
2023-03-08 18:58:58 +0900

    wip

--- src/pages/BoardList.tsx
+++ src/pages/BoardList.tsx
@@ -1,14 +1,30 @@
 import "../styles/BoardList.css";
 import { BoardListProps } from "../common";
 import { ListCard } from "./ListCard";
+import { Droppable } from "react-beautiful-dnd";
 
 export function BoardList({ id, title, entities }: BoardListProps) {
   return (
-    <div className="board-list">
-      {title}
-      {entities.map(({ id, title, description }) => (
-        <ListCard id={id} title={title} description={description} />
-      ))}
-    </div>
+    <Droppable droppableId={id}>
+      {(provided) => (
+        <div
+          key={id}
+          className="board-list"
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+        >
+          {title}
+          {entities.map(({ id, title, description }, index) => (
+            <ListCard
+              id={id}
+              title={title}
+              description={description}
+              index={index}
+            />
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
   );
 }
