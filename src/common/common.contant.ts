4fbdf6ff659b86d8de0edcec8d574402b49f70c9
psh0706 <best8427@naver.com>
2023-03-08 13:41:40 +0900

    리스트 데이터 구조를 위한 타입과 상수를 정의

---
+++ src/common/common.contant.ts
@@ -0,0 +1,17 @@
+export const tempListData = [
+  {
+    id: 1,
+    title: "To Do",
+    entities: [],
+  },
+  {
+    id: 2,
+    title: "Doing",
+    entities: [],
+  },
+  {
+    id: 3,
+    title: "Done",
+    entities: [],
+  },
+];
---
+++ src/common/common.type.ts
@@ -0,0 +1,13 @@
+// 한 리스트가 가지는 타입
+export type BoardList = {
+  id: number;
+  title: string;
+  entities: Card[];
+};
+
+// 한 카드가 가지는 타입
+export type Card = {
+  id: number;
+  title: string;
+  description: string;
+};
