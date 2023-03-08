4807e6a04b384a920b9960cc54ec60165e0e1531
psh0706 <best8427@naver.com>
2023-03-08 18:22:40 +0900

    refactoring

--- src/pages/BoardList.tsx
+++ src/pages/BoardList.tsx
@@ -1,6 +1,14 @@
 import "../styles/BoardList.css";
 import { BoardListProps } from "../common";
+import { ListCard } from "./ListCard";
 
 export function BoardList({ id, title, entities }: BoardListProps) {
-  return <div className="board-list">{title}</div>;
+  return (
+    <div className="board-list">
+      {title}
+      {entities.map(({ id, title, description }) => (
+        <ListCard id={id} title={title} description={description} />
+      ))}
+    </div>
+  );
 }
